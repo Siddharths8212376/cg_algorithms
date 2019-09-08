273b5f69bc10353f552b95403d162da1d44ed0b8
Siddharths8212376 <siddharth8212376@gmail.com>
2019-09-09 03:51:45 +0530

    updated the __init__.py file

--- cg_algorithms/__init__.py
+++ cg_algorithms/__init__.py
@@ -2,9 +2,9 @@
 
 """Top-level package for cg_algos."""
 # adding the import statements
-from circle_algorithms import circle_algorithms
-from line_algorithm import line_algorithm
-from ellipse_algo import ellipse_algo
+from .circle_algorithms import circle_algorithms
+from .line_algorithm import line_algorithm
+from .ellipse_algo import ellipse_algo
 
 __author__ = """Siddharth Suresh"""
 __email__ = 'siddharth8212376@gmail.com'
