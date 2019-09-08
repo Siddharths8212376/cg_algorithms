76e0a56cc1ea22121b879c89d2ca309d42f3f1c1
Siddharths8212376 <siddharth8212376@gmail.com>
2019-09-09 03:41:31 +0530

    adding version control

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Siddharths8212376/cg_algorithms',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
