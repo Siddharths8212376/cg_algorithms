2be633f352e01fe5dcd45bbe1fc739cbed7cc8a3
Siddharths8212376 <siddharth8212376@gmail.com>
2019-09-09 12:23:22 +0530

    Updated the cli.py file

--- cg_algorithms/cli.py
+++ cg_algorithms/cli.py
@@ -2,15 +2,17 @@
 
 """Console script for cg_algorithms."""
 import sys
-import click
+from __future__ import print_function
+# import click
 
 
 @click.command()
 def main(args=None):
     """Console script for cg_algorithms."""
-    click.echo("Replace this message by putting your code into "
-               "cg_algorithms.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    # click.echo("Replace this message by putting your code into "
+    #            "cg_algorithms.cli.main")
+    # click.echo("See click documentation at http://click.pocoo.org/")
+    print('Hello Everyone!!!')
     return 0
 
 
